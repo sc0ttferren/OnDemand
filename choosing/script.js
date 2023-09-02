b8326b5d1646d89d64690c2c87fcc09d13b085c9
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-02 11:33:02 -0500

    Create script.js

---
+++ choosing/script.js
@@ -0,0 +1,11 @@
+// Add click event listener to the MovieScape button
+document.getElementById('moviescape-button').addEventListener('click', function () {
+    // Redirect to MovieScape or perform any desired action
+    window.location.href = 'https://scottstheatres.com/moviescape'; // Replace with your actual URL
+});
+
+// Add click event listener to the TVRealm button
+document.getElementById('tvrealm-button').addEventListener('click', function () {
+    // Redirect to TVRealm
+    window.location.href = 'https://scottstheatres.com/TVRealm'; // Replace with your actual URL
+});
