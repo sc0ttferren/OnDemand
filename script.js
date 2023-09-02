7f0a54e057498e0290fc6f5a35a540b55cdc6af3
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-02 11:55:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Add click event listener to the MovieScape button
 document.getElementById('moviescape-button').addEventListener('click', function () {
     // Redirect to MovieScape or perform any desired action
-    window.location.href = 'https://scottstheatres.vercel.app/moviescape'; // Replace with your actual URL
+    window.location.href = 'https://scottstheatres.vercel.app/moviescape/'; // Replace with your actual URL
 });
 
 // Add click event listener to the TVRealm button
 document.getElementById('tvrealm-button').addEventListener('click', function () {
     // Redirect to TVRealm
-    window.location.href = 'https://scottstheatres.vercel.app/TVRealm'; // Replace with your actual URL
+    window.location.href = 'https://scottstheatres.vercel.app/TVRealm/'; // Replace with your actual URL
 });
