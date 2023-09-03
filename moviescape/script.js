40b24e89ba644634320161d45e3c8b6211fe02f8
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-02 22:38:58 -0500

    Update script.js

--- moviescape/script.js
+++ moviescape/script.js
@@ -1,6 +1,6 @@
 // script.js
 const movies = [
-    { title: 'The Super Mario Bros', year: 2021, image: 'https://github-production-user-asset-6210df.s3.amazonaws.com/100329425/265240578-6df0b244-afa9-4df8-9297-3cd0d8362900.jpg', link: 'movie1.html' },
+    { title: 'The Super Mario Bros', year: 2021, image: 'movie1.jpg', link: 'movie1.html' },
     { title: 'Movie 2', year: 2022, image: 'movie2.jpg', link: 'movie2.html' },
     { title: 'Movie 3', year: 2023, image: 'movie3.jpg', link: 'movie3.html' },
 ];
