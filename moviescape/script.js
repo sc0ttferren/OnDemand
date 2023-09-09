ded7b74b83cc6cb575739c9a8c446628ae34463a
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-08 21:28:36 -0500

    Update script.js

--- moviescape/script.js
+++ moviescape/script.js
@@ -1,39 +1,44 @@
 // script.js
-const movies = [
-    { title: 'The Super Mario Bros', genre: 'Adventure', image: 'movie1.jpg', link: 'movies/the-super-mario-bros' },
-    { title: 'Monsters University', genre: 'Comedy', image: 'movie2.jpg', link: 'movies/monsters-university' },
-    { title: 'Spiderman: Across The SpiderVerse', genre: 'Action', image: 'movie3.jpg', link: 'movies/spiderman-across-the-spiderverse' },
-    { title: 'Blue Beetle', genre: 'Action', image: 'movie3.jpg', link: 'movies/blue-beetle' },
-    { title: 'Cars', genre: 'Action', image: 'movie3.jpg', link: 'movies/cars' },
-        { title: 'Cars', genre: 'Action', image: 'movie3.jpg', link: 'movies/cars' },
-        { title: 'Cars', genre: 'Action', image: 'movie3.jpg', link: 'movies/cars' },
-        { title: 'Cars', genre: 'Action', image: 'movie3.jpg', link: 'movies/cars' },
-];
 
-const movieList = document.getElementById('movie-list');
+// Function to check if a cookie with the given name exists
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
 
-// Function to generate a random 5-character string
-function generateRandomValue() {
+const userpermKey = getCookie('userpermKey');
+
+if (!userpermKey) {
+    // Cookie doesn't exist, so generate and set it
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < 5; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         result += characters.charAt(randomIndex);
     }
-    return result;
-}
 
-// Function to set a cookie with the generated value
-function setCookie(name, value, days) {
     const expires = new Date();
     expires.setFullYear(expires.getFullYear() + 50); // Set expiration to 50 years from now
-    const cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;domain=.scottstheatres.vercel.app`;
+    const cookie = `userpermKey=${result};expires=${expires.toUTCString()};path=/;domain=.scottstheatres.vercel.app`;
     document.cookie = cookie;
 }
 
-// Generate a random value and set it as a cookie
-const userpermKey = generateRandomValue();
-setCookie('userpermKey', userpermKey, 18250); // 18250 days (50 years) expiration
+// The rest of your code for creating movie cards
+const movies = [
+    { title: 'The Super Mario Bros', genre: 'Adventure', image: 'movie1.jpg', link: 'movies/the-super-mario-bros' },
+    { title: 'Monsters University', genre: 'Comedy', image: 'movie2.jpg', link: 'movies/monsters-university' },
+    { title: 'Spiderman: Across The SpiderVerse', genre: 'Action', image: 'movie3.jpg', link: 'movies/spiderman-across-the-spiderverse' },
+    { title: 'Blue Beetle', genre: 'Action', image: 'movie3.jpg', link: 'movies/blue-beetle' },
+    { title: 'Cars', genre: 'Action', image: 'movie3.jpg', link: 'movies/cars' },
+];
+
+const movieList = document.getElementById('movie-list');
 
 movies.forEach(movie => {
     const movieCard = document.createElement('div');
