c92ca7efd09f8cd94f620743a50fc64edf3eb0c8
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-09 12:33:57 -0500

    Delete moviescape/settings-ui.js

--- moviescape/settings-ui.js
+++
@@ -1,9 +0,0 @@
-// settings-ui.js
-
-const settingsIcon = document.getElementById('settings-icon');
-const settingsMenu = document.getElementById('settings-menu');
-
-// Toggle the settings menu when the settings icon is clicked
-settingsIcon.addEventListener('click', () => {
- settingsMenu.classList.toggle('active');
-});
