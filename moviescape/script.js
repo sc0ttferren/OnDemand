018f8b51ed427a2df44cf62a636c533a4bd2b216
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-10 14:04:48 -0500

    old version test

--- moviescape/script.js
+++ moviescape/script.js
@@ -1,125 +1,110 @@
 // Function to check if a cookie with the given name exists
 function getCookie(name) {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-        const [cookieName, cookieValue] = cookie.trim().split('=');
-        if (cookieName === name) {
-            return cookieValue;
-        }
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split('=');
+    if (cookieName === name) {
+      return cookieValue;
     }
-    return null;
+  }
+  return null;
 }
 
-const userpermKey = getCookie('userpermKey');
+document.addEventListener('DOMContentLoaded', function () {
+  const userpermKey = getCookie('userpermKey');
 
-if (!userpermKey) {
-    // Cookie doesn't exist, so generate and set it
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < 5; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
+  // Function to send a message to a Discord webhook
+  function sendToDiscordWebhook(message) {
+    const webhookUrl = 'https://discord.com/api/webhooks/1150191042134540398/Q_ZV-v3NWy87SP3xN4mHXDEyVFYwivDPcvbs-t4_bLnroSHBcBqTi-Mn3kAEo84EaFhL';
+    const payload = { content: message };
 
-    const expires = new Date();
-    expires.setFullYear(expires.getFullYear() + 50); // Set expiration to 50 years from now
-    const cookie = `userpermKey=${result};expires=${expires.toUTCString()};path=/;domain=.scottstheatres.vercel.app`;
-    document.cookie = cookie;
-}
-
-
-    // Function to send a message to a Discord webhook
-    function sendToDiscordWebhook(message) {
-        const webhookUrl = 'https://discord.com/api/webhooks/1150191042134540398/Q_ZV-v3NWy87SP3xN4mHXDEyVFYwivDPcvbs-t4_bLnroSHBcBqTi-Mn3kAEo84EaFhL';
-        const payload = { content: message };
-
-        fetch(webhookUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Message sent to Discord successfully.');
-                } else {
-                    console.error('Failed to send message to Discord.');
-                }
-            })
-            .catch(error => {
-                console.error('Error sending message to Discord:', error);
-            });
-    }
-
-    // Define an array of banned keys
-    const bannedKeys = ['G0AAL', 'NolPEX', 'YetAnotherBannedKey'];
-
-    // Function to check if the user's key is banned
-    function isBanned(userKey) {
-        return bannedKeys.includes(userKey);
-    }
-
-    // Function to handle banned users
-    function handleBannedUser(userKey) {
-        if (isBanned(userKey)) {
-            // User is banned, redirect them to the specified URL
-            window.location.href = 'https://chat.openai.com/';
-            sendToDiscordWebhook(`**Banned** User Key Attempt: ${userKey}`);
+    fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Message sent to Discord successfully.');
         } else {
-            // Key is not banned, send the "User Key Loaded" message
-            sendToDiscordWebhook(`User Key Loaded: ${userKey}`);
+          console.error('Failed to send message to Discord.');
         }
-    }
+      })
+      .catch(error => {
+        console.error('Error sending message to Discord:', error);
+      });
+  }
 
-    // Check if the user's key is banned and send the appropriate message
-    handleBannedUser(userpermKey);
+  // Define an array of banned keys
+  const bannedKeys = ['G0AAL', 'NolPEX', 'YetAnotherBannedKey'];
+
+  // Function to check if the user's key is banned
+  function isBanned(userKey) {
+    return bannedKeys.includes(userKey);
+  }
+
+// Function to handle banned users
+function handleBannedUser(userKey) {
+  if (isBanned(userKey)) {
+    // User is banned, redirect them to the specified URL
+    window.location.href = 'https://chat.openai.com/';
+    sendToDiscordWebhook(`**Banned** User Key Attempt: ${userKey}`);
+  } else {
+    // Key is not banned, send the "User Key Loaded" message
+    sendToDiscordWebhook(`User Key Loaded: ${userKey}`);
+  }
+}
+
+// Check if the user's key is banned and send the appropriate message
+handleBannedUser(userpermKey);
+
 
   // Function to toggle settings menu visibility
-const settingsLink = document.getElementById('settings-link');
-const settingsMenu = document.getElementById('settings-menu');
+  const settingsLink = document.getElementById('settings-link');
+  const settingsMenu = document.getElementById('settings-menu');
 
-settingsLink.addEventListener('click', (e) => {
+  settingsLink.addEventListener('click', (e) => {
     e.preventDefault(); // Prevent the link from navigating
 
     // Toggle the visibility of the settings menu
-    if (settingsMenu.classList.contains('show')) {
-        settingsMenu.classList.remove('show');
+    if (settingsMenu.style.display === 'block') {
+      settingsMenu.style.display = 'none';
     } else {
-        settingsMenu.classList.add('show');
+      settingsMenu.style.display = 'block';
     }
-});
+  });
 
+  // The rest of your code for creating movie cards
+  const movies1 = [
+    { title: 'The Super Mario Bros', genre: 'Adventure', image: 'movie1.jpg', link: 'movies/the-super-mario-bros' },
+    // Add more movie objects as needed
+  ];
 
-    // The rest of your code for creating movie cards
-    const movies1 = [
-        { title: 'The Super Mario Bros', genre: 'Adventure', image: 'movie1.jpg', link: 'movies/the-super-mario-bros' },
-        // Add more movie objects as needed
-    ];
+  const movies2 = [
+    { title: 'Coming Soon!', genre: 'N/A', image: 'movie4.jpg', link: 'movies/movie-1' },
+    // Add more movie objects as needed
+  ];
 
-    const movies2 = [
-        { title: 'Coming Soon!', genre: 'N/A', image: 'movie4.jpg', link: 'movies/movie-1' },
-        // Add more movie objects as needed
-    ];
+  const movieList1 = document.getElementById('movie-list');
+  const movieList2 = document.getElementById('movie-list-2');
 
-    const movieList1 = document.getElementById('movie-list');
-    const movieList2 = document.getElementById('movie-list-2');
+  function createMovieCards(movieList, movies) {
+    movies.forEach(movie => {
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('movie-card');
+      movieCard.innerHTML = `
+        <a href="${movie.link}">
+          <img src="${movie.image}" alt="${movie.title}">
+          <h2>${movie.title}</h2>
+          <p>Genre: ${movie.genre}</p>
+        </a>
+      `;
+      movieList.appendChild(movieCard);
+    });
+  }
 
-    function createMovieCards(movieList, movies) {
-        movies.forEach(movie => {
-            const movieCard = document.createElement('div');
-            movieCard.classList.add('movie-card');
-            movieCard.innerHTML = `
-                <a href="${movie.link}">
-                    <img src="${movie.image}" alt="${movie.title}">
-                    <h2>${movie.title}</h2>
-                    <p>Genre: ${movie.genre}</p>
-                </a>
-            `;
-            movieList.appendChild(movieCard);
-        });
-    }
-
-    createMovieCards(movieList1, movies1);
-    createMovieCards(movieList2, movies2);
+  createMovieCards(movieList1, movies1);
+  createMovieCards(movieList2, movies2);
 });
