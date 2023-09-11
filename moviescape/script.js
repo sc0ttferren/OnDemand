9de7321b06dd782bfa5515a3cd433532fa191169
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-09-11 09:38:17 -0500

    Update script.js

--- moviescape/script.js
+++ moviescape/script.js
@@ -61,8 +61,8 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   }
 
-  // Define an array of banned keys
-  const bannedKeys = ['G0AAL', '81654', 'plslemmebanyou'];
+  // Define an array of banned keys (81654) - david admin panel key later (remove this)
+  const bannedKeys = ['G0AAL', '81653', 'plslemmebanyou'];
 
   // Function to check if the user's key is banned
   function isBanned(userKey) {
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function handleBannedUser(userKey) {
   if (isBanned(userKey)) {
     // User is banned, redirect them to the specified URL
-    window.location.href = 'https://roblocky.vercel.app/1jayd0gA10BHQsJ54fBO1g1O0SxazzUs0O0zB/bludjustdidthemugsh0tb01HALtes20c3ewd.html';
+    window.location.href = 'https://chat.openai.com/';
     sendToDiscordWebhook(`**Banned** User Key Attempt: ${userKey}`);
   } else {
     // Key is not banned, send the "User Key Loaded" message
